9e320072fd6311b94058adf76726f0387985a0e2
Claireecf <89868984+Claireecf@users.noreply.github.com>
2022-11-01 11:08:23 -0500

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,10 @@ use std::env;
 use isahc::prelude::*;
 use isahc::{config::SslOption};
 
-use opentelemetry::{global, sdk::export::trace::stdout, trace::Tracer};
+use opentelemetry::{global, trace::{Span, Tracer}, KeyValue};
+use tracing::{error, span};
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::Registry;
 
 use isahc::{
     config::VersionNegotiation,
@@ -19,7 +22,14 @@ fn main() -> Result<(), isahc::Error>   {
     // Must be `mut` so we can read the response body.
     // HTTP/2 with prior knowledge.
     global::set_text_map_propagator(opentelemetry_jaeger::Propagator::new());
-    let tracer = opentelemetry_jaeger::new_agent_pipeline().install_simple().unwrap();
+    let tracer = opentelemetry_jaeger::new_agent_pipeline().with_service_name("http2_client").install_simple().unwrap();
+
+    // Create a tracing layer with the configured tracer
+    let telemetry = tracing_opentelemetry::layer().with_tracer(tracer.clone());
+    // Use the tracing subscriber `Registry`, or any other subscriber
+    // that impls `LookupSpan`
+    let subscriber = Registry::default().with(telemetry);
+
     let args: Vec<String> = env::args().collect();
     let url = &args[1];
     // let mut file = OpenOptions::new().write(true).read(true).append(true).create(true).open("time.txt").unwrap();
@@ -29,19 +39,30 @@ fn main() -> Result<(), isahc::Error>   {
     // file.write_all(content.as_bytes()).unwrap();
     // file.write_all(datetime.format("%m/%d/%Y %T%.3f\n").to_string().as_bytes()).unwrap();
 
-    tracer.in_span("doing_work", |cx| {
-    let http2_client = HttpClient::builder()
-    .version_negotiation(VersionNegotiation::http2())
-    .ssl_options(SslOption::DANGER_ACCEPT_INVALID_CERTS | SslOption::DANGER_ACCEPT_REVOKED_CERTS | SslOption::DANGER_ACCEPT_INVALID_HOSTS)
-    .build().unwrap();
-    let mut response = http2_client.get(url).unwrap();
-    // Print some basic info about the response to standard output.
-    println!("Status: {}", response.status());
-    println!("Headers: {:#?}", response.headers());
-    // Read the response body as text into a string and print it.
-    print!("{}", response.text().unwrap());
-    });
+    // start a new span
+    let mut span = tracer.start("http2_fetch_webpage");
 
+    // Trace executed code
+    tracing::subscriber::with_default(subscriber, || {
+        // Spans will be sent to the configured OpenTelemetry exporter
+        let root = span!(tracing::Level::TRACE, "app_start", work_units = 2);
+        let _enter = root.enter();
+        let http2_client = HttpClient::builder()
+        .version_negotiation(VersionNegotiation::http2())
+        .ssl_options(SslOption::DANGER_ACCEPT_INVALID_CERTS | SslOption::DANGER_ACCEPT_REVOKED_CERTS | SslOption::DANGER_ACCEPT_INVALID_HOSTS)
+        .build().unwrap();
+
+
+        let mut response = http2_client.get(url).unwrap();
+        // Print some basic info about the response to standard output.
+        println!("Status: {}", response.status());
+        println!("Headers: {:#?}", response.headers());
+        // Read the response body as text into a string and print it.
+        print!("{}", response.text().unwrap());
+        error!("This event will be logged in the root span.");
+    });
+    // end or drop the span to export
+    span.end();
     // export remaining spans
     global::shutdown_tracer_provider(); 
 
